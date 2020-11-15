87dd994872cbd2bfafd736cb69de24b5a994cf27
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-15 14:36:54 -0300

    Finalizado AppMembrosEstaticos

---
+++ AppMembrosEstaticos/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMembrosEstaticos
+{
+    public class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double CalcularCircunferencia(double raio)
+        {
+            return 2 * Pi * raio;
+        }
+
+        public static double CalcularVolume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
+        }
+    }
+}
---
+++ AppMembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AppMembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = 3.00;
+
+            double circunferencia = Calculadora.CalcularCircunferencia(raio);
+            Console.WriteLine("Circunferência: " + circunferencia);
+
+            double volume = Calculadora.CalcularVolume(raio);
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi);
+
+            Console.ReadKey();
+        }        
+    }
+}
