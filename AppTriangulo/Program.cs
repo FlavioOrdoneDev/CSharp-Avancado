ddedb7007b2c2519def9abadd756b675d19b38eb
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-15 12:28:07 -0300

    Adicionado o projeto AppTriangulo

---
+++ AppTriangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AppTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X = new Triangulo();
+            Triangulo Y = new Triangulo();
+
+            double areaX = X.CalcularArea(3, 4, 5);
+            double areaY = Y.CalcularArea(7.5, 4.5, 4.02);            
+
+            Console.WriteLine("Area de X = " + areaX.ToString("F4"));
+            Console.WriteLine("Area de Y = " + areaY.ToString("F4"));
+
+            if (areaX > areaY)
+                Console.WriteLine("Maior area: X");
+            else
+                Console.WriteLine("Maior area: X");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AppTriangulo/Triangulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTriangulo
+{
+    public class Triangulo
+    {
+        public double CalcularArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2.0;
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
