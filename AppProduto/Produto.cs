78e19243cea61f7a4784ba1908749f4d2543e36f
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-16 12:19:26 -0300

    Adicionado projeto de construtores e encapsulamento

---
+++ AppProduto/Produto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppProduto
+{
+    public class Produto
+    {
+        public Produto() { }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            if (quantidade <= 0)
+                Console.WriteLine("Insira uma quantidade válida");
+            else
+                Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            if (Quantidade < quantidade)
+                Console.WriteLine("Não será possivel remover essa quantidade");
+            else
+                Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Dados do Produto: " + Nome + ", R$ " + Preco.ToString("F2") + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
---
+++ AppProduto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AppProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto("TV", 900.00, 10);
+            Console.WriteLine(produto1);
+
+            produto1.AdicionarProdutos(5);
+            Console.WriteLine(produto1);
+
+            produto1.RemoverProdutos(3);
+            Console.WriteLine(produto1);
+
+            Console.ReadKey();
+        }
+    }
+}
