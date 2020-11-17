8d93712ed971dcbe6c49ca56d5b65c13b2336619
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-17 01:42:19 -0300

    Finalizado projetos de Listas e Arrays

---
+++ AppHashSet/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppHashSet
+{
+    class Program
+    {
+        static HashSet<int> A;
+        static HashSet<int> B;
+
+        static void Main(string[] args)
+        {
+            A = new HashSet<int>();
+            B = new HashSet<int>();
+
+            A.Add(3);
+            A.Add(5);
+            A.Add(8);
+            A.Add(9);
+
+            //A.Remove(8);
+
+            B.Add(3);
+            B.Add(4);
+            B.Add(5);
+
+            A.ExceptWith(B); // Diferênça entre conjuntos
+            MostrarConjunto(A);
+
+            A.UnionWith(B); // União entre conjuntos
+            MostrarConjunto(A);
+
+            A.IntersectWith(B); // O quê é igual entre os conjuntos
+            MostrarConjunto(A);
+
+
+            // B.Remove(4);
+
+            VerificaPertence(4);
+            VerificaPertence(7);
+
+            Console.ReadKey();
+        }
+
+        public static void VerificaPertence(int numero)
+        {
+            if (B.Contains(numero))
+                Console.WriteLine("Pertence: " + numero);
+            else
+                Console.WriteLine("Não Pertence: " + numero);
+        }
+
+        public static void MostrarConjunto(HashSet<int> conjunto)
+        {
+            foreach (var item in conjunto)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AppMatrizes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AppMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat = new int[3, 3] { { 5, -3, 10 }, { 15, 8, 2 }, { 7, 9, -4 } };
+
+            int negativo = 0;
+            string diagonalPrincipal = "";
+
+            Console.WriteLine("Diagonal:");
+            for (int x = 0; x < 3; x++)
+            {
+                Console.Write(mat[x, x] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (i == j)
+                        diagonalPrincipal += mat[i, j] + " ";
+                    
+                    if (mat[i, j] < 0)
+                        negativo++;
+                }
+            }
+
+            Console.WriteLine("Diagonal: " + diagonalPrincipal);
+            Console.WriteLine("Negativos: " + negativo);
+            Console.ReadKey();
+        }
+    }
+}
