306fc8906cb911735b275529b7c52ebdea75be68
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-23 15:03:33 -0300

    Finalizacao do projeto de Interfaces

---
+++ AppComparable/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Funcionario> lista = new List<Funcionario>();
+
+            lista.Add(new Funcionario("Maria Brown", 4300.00));
+            lista.Add(new Funcionario("Alex Green", 3100.00));
+            lista.Add(new Funcionario("Bob Grey", 3100.00));
+            lista.Add(new Funcionario("Anna White", 3500.00));
+            lista.Add(new Funcionario("Alex Black", 2450.00));
+            lista.Add(new Funcionario("Eduardo Rose", 4390.00));
+            lista.Add(new Funcionario("William Red", 2900.00));
+            lista.Add(new Funcionario("Marta Blue", 6100.00));
+            lista.Add(new Funcionario("Alex Brown", 5000.00));
+
+            lista.Sort();
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Funcionario : IComparable
+    {
+        public Funcionario(string nome, double salario)
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Funcionario))
+                throw new ArgumentException("Comparação inválida");
+            Funcionario funcionario = obj as Funcionario;
+            return Nome.CompareTo(funcionario.Nome);
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + ", Salário: " + Salario.ToString("F2");
+        }
+    }
+}
---
+++ AppComparison/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
+
+            products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); // Comparison<T> comparison
+            //products.Sort((p1, p2) => p1.Price.CompareTo(p2.Price));
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Price: " + Price.ToString("F2");
+        }
+    }
+}
